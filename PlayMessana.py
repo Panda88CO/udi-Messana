0bebf63a88fec128271cd8860b39299076071584
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2020-06-17 23:39:30 -0700

    Update PlayMessana.py

--- PlayMessana.py
+++ PlayMessana.py
@@ -1,12 +1,9 @@
-
 import os
+import sys
 import glob
 import time
 import datetime
-#import RPi.GPIO as iO
 import os,subprocess
-from subprocess import call
-#from w1thermsensor import W1ThermSensor
 import json
 import requests
 
@@ -14,11 +11,20 @@ import requests
 
 messanaAPIKey = 'apikey=9bf711fc-54e2-4387-9c7f-991bbb02ab3a'
 MessanaIP = 'http://192.168.2.45'
-systemStr = '/api/system/'
+systemStr = '/api/system'
 zoneStr = '/api/zone'
+GetStr =MessanaIP+systemStr+'/zoneCount/?'+ messanaAPIKey
 
 
-
-nbrZones =  requests.get(MessanaIP+systemStr+'/zoneCount/?'+ messanaAPIKey )
+zones =  requests.get(GetStr).json()
+nbrZone = zones['count']
+i=0
+namedZones=[]
+for i in range(0, nbrZones):
+    GetStr =MessanaIP+zoneStr+'/name/'+str(i)+'?'+ messanaAPIKey
+    namedZones[i] =  requests.get(GetStr).json()
+    GetStr =MessanaIP+zoneStr+'/setPoint/'+str(i)+'?'+ messanaAPIKey
+    print(namedZones[i]['value'])
+print()
 
 
